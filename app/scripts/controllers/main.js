6d80f9c48b5ac2e3c7773a4b444ea962eee258ac
Luke Pommersheim <lpommers@gmail.com>
2014-08-19 09:08:58 +0200

    got the basics working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,18 +10,24 @@
 angular.module('someAppApp')
   .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
+  	var cuisines = ['american', 'italian', 'isian', 'mexican', 'southern & soul food', 'french', 'southwestern', 'barbecue', 'indian', 'chinese', 'Cajun & Creole', 'english', 'mediterranean', 'greek', 'spanish', 'german', 'thai', 'moroccan', 'irish', 'japanese', 'cuban', 'hawaiin', 'swedish', 'hungarian', 'portugese'];
+
   	$scope.todos = [];
   	$scope.someProperty = "hello computer";
 
   	$scope.someAction = function() {
-	  	var url = 'http://api.yummly.com/v1/api/recipes?_app_id=09981ac4&_app_key=5f7f4b7a4eb4ebf0bcf4f3f0e5e47e2f&callback=JSON_CALLBACK';
+  		var randomCuisine = encodeURIComponent(cuisines[Math.floor(Math.random() * (cuisines.length + 1) )]);
+  		console.log(randomCuisine);
+	  	var url = 'http://api.yummly.com/v1/api/recipes?_app_id=09981ac4&_app_key=5f7f4b7a4eb4ebf0bcf4f3f0e5e47e2f&allowedCuisine[]=cuisine^cuisine-' + randomCuisine + '&callback=JSON_CALLBACK';
+	  	console.log(url);
 			var promise = $http.jsonp(url);
 
 			promise.then(function(response) {
 				var results = response.data.matches;
-				console.log(response);
 
-				$scope.someProperty = results;
+
+				$scope.someProperty = results[0];
+
 
 			});
   	}
