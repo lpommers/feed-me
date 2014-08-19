752fe674a95ecf97dbfc8e263d39874ad4f04bca
Luke Pommersheim <lpommers@gmail.com>
2014-08-19 15:10:56 +0200

    first working instance of not having to call the API every time

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,21 +8,22 @@
  * Controller of the someAppApp
  */
 angular.module('someAppApp')
-  .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
+  .controller('MainCtrl', ['$scope','$http', function ($scope, $http) {
 
-  	var cuisines = ['american', 'italian', 'asian', 'mexican', 'southern & soul food', 'french', 'southwestern', 'barbecue', 'indian', 'chinese', 'Cajun & Creole', 'english', 'mediterranean', 'greek', 'spanish', 'german', 'thai', 'moroccan', 'irish', 'japanese', 'cuban', 'hawaiin', 'swedish', 'hungarian', 'portugese'];
+
 
 
   	var recipeIndexes =  recipeIndexArr();
   	console.log(recipeIndexes);
   	var counter = 0;
-  	console.log("counter: " + counter);
-  	var recipes;
+  	console.log('counter: ' + counter);
+  	var recipes= [];
   	$scope.someProperty = '';
   	$scope.someOtherProperty = [];
 
   	$scope.someAction = function() {
-  		console.log("counter: " + counter);
+  		console.log('counter: ' + counter);
+
   		if (counter === 95 || counter === 0) {
   			//reset
   			counter = 0;
@@ -36,24 +37,27 @@ angular.module('someAppApp')
 				var promise = $http.jsonp(url);
 
 				promise.then(function(response) {
-					$scope.someOtherProperty = response.data.matches;
-					console.log($scope.someOtherProperty);
+					recipes = response.data.matches;
+					$scope.someProperty = response.data.matches[0];
+					//console.log($scope.someOtherProperty);
 				});
 
-
+  		} else {
+  			var randomIndex = recipeIndexes.splice(Math.random()*recipeIndexes.length, 1)[0];
+  			$scope.someProperty = recipes[randomIndex];
   		}
-  		console.log($scope.someOtherProperty);
+  		// console.log($scope.someOtherProperty);
   		counter += 1;
-  		var randomIndex = recipeIndexes.splice(Math.random()*recipeIndexes.length, 1)[0];
-  		console.log(randomIndex);
-  		console.log($scope.someOtherProperty[randomIndex]);
-  		$scope.someProperty = $scope.someOtherProperty[randomIndex];
+
+  		console.log(recipes);
   		console.log($scope.someProperty);
   	};
 
 
 
   }]);
+
+
 
 function recipeIndexArr () {
 	var recipeIndexes = [];
