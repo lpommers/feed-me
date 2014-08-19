80a9dd2cc92b06af0b350d235b0c694e6952126f
Luke Pommersheim <lpommers@gmail.com>
2014-08-19 08:11:37 +0200

    got the basics working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,14 +10,25 @@
 angular.module('someAppApp')
   .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
 
-		var url = 'http://api.yummly.com/v1/api/recipes?_app_id=09981ac4&_app_key=5f7f4b7a4eb4ebf0bcf4f3f0e5e47e2f&allowedIngredient[]=rice&callback=JSON_CALLBACK';
-		var promise = $http.jsonp(url);
+  	$scope.todos = [];
+  	$scope.someProperty = "hello computer";
 
-		promise.then(function(response) {
-			var results = response.data.matches;
-			console.log(response);
+  	$scope.someAction = function() {
+	  	var url = 'http://api.yummly.com/v1/api/recipes?_app_id=09981ac4&_app_key=5f7f4b7a4eb4ebf0bcf4f3f0e5e47e2f&callback=JSON_CALLBACK';
+			var promise = $http.jsonp(url);
 
-			$scope.todos = results;
+			promise.then(function(response) {
+				var results = response.data.matches;
+				console.log(response);
 
-		});
+				$scope.someProperty = results;
+
+			});
+  	}
+
+
+
   }]);
+
+
+
