0dec0463778b34f41c4f7a3552a98d72a6ace5d8
Luke Pommersheim <lpommers@gmail.com>
2014-08-20 17:54:33 +0200

    comments and fixing a part where I was making two api calls when the counter got reset

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,7 +9,7 @@ function recipeIndexArr (num) {
 	return recipeIndexes;
 }
 
-//returns a random number from 0 - 300
+//returns a random number from 0 - 2000 to get varied results
 function randomStart () {
 	return Math.floor(Math.random() * (2000) );
 }
@@ -19,7 +19,7 @@ function buildURL() {
 	var random = randomStart(),
 			mainDishes = encodeURIComponent('Main Dishes'),
 			salads = encodeURIComponent('Salads');
-	return 'http://api.yummly.com/v1/api/recipes?_app_id=09981ac4&_app_key=5f7f4b7a4eb4ebf0bcf4f3f0e5e47e2f&allowedCourse[]=course^course-' + mainDishes + '&allowedCourse[]=course^course-' + salads + '&maxResult=60&start='+ random +'&callback=JSON_CALLBACK';
+	return 'http://api.yummly.com/v1/api/recipes?_app_id=09981ac4&_app_key=5f7f4b7a4eb4ebf0bcf4f3f0e5e47e2f&allowedCourse[]=course^course-' + mainDishes + '&allowedCourse[]=course^course-' + salads + '&maxResult=80&start='+ random +'&callback=JSON_CALLBACK';
 }
 /**
  * @ngdoc function
@@ -31,54 +31,51 @@ function buildURL() {
 
 angular.module('someAppApp')
   .controller('MainCtrl', ['$scope','$http', function ($scope, $http) {
+
+  	//declare some variables
   	$scope.someProperty = '';
   	$scope.htmlAttr = '';
   	var recipes = [],
-  			recipeIndexes = recipeIndexArr(60),
+  			recipeIndexes = recipeIndexArr(80),
 			  counter = 0;
 
   	console.log('counter: ' + counter);
   	console.log(recipeIndexes);
 
+  	//function to make a call to the yummly api. once the promise is complete, then:
+  	//recipes = all the recipes
+  	// scope.someProperty = first recipe from the response
   	$scope.apiCall = function (){
-
-  		 var promise = $http.jsonp(buildURL());
-  			promise.then(function(response) {
-  			  console.log(response.data);
-					recipes = response.data.matches;
-					console.log(recipes);
-					$scope.someProperty = response.data.matches[0];
-					$scope.htmlAttr = response.data.attribution.html;
-				});
+			var promise = $http.jsonp(buildURL());
+			promise.then(function(response) {
+			  console.log(response.data);
+				recipes = response.data.matches;
+				console.log(recipes);
+				$scope.someProperty = response.data.matches[0];
+				$scope.htmlAttr = response.data.attribution.html;
+			});
   	};
 
+  	//this is the button click
   	$scope.someAction = function() {
-  		var promise;
   		console.log('counter: ' + counter);
 			console.log(recipeIndexes);
-  		if (counter === 0) {
 
-
-				$scope.apiCall(promise);
+			//if recipes array is empty, it means the api has never called so, so we call it once. And update the counter
+  		if (recipes.length === 0) {
+				$scope.apiCall();
 				counter += 1;
   		}
+  		//if the counter is at 78 then we've shown the user almost all the recipes from our response and it's time to update the recipes by making another api call and refreshing the recipe index array and reseting our counter
+			else if (counter === 78) {
+				recipeIndexes = recipeIndexArr(80 );
+			 	$scope.apiCall();
+				counter = 0;
 
-			else if (counter === 55) {
-			//reset
-			//
-				recipeIndexes = recipeIndexArr(60);
-
-			 	promise = $http.jsonp(buildURL());
-
-				promise.then(function(response) {
-					recipes = response.data.matches;
-					console.log(recipes);
-					$scope.someProperty = response.data.matches[0];
-
-				});
-				counter += 1;
-
-  		} else {
+  		}
+  		//this means we don't need to call the api and just need to send the user a new recipe
+  		else {
+  			//get a random number, that we won't call more than once for each api call
   			var randomIndex = recipeIndexes.splice(Math.random()*recipeIndexes.length, 1)[0];
   			$scope.someProperty = recipes[randomIndex];
   			counter += 1;
